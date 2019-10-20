f59272f687ba98fe4c2631a21c750878f17c6870
Mariia Turchina <mariia.turchina@gmail.com>
2019-10-20 15:20:57 +0200

    prevented from always playing right

--- BlindShot/ViewController.swift
+++ BlindShot/ViewController.swift
@@ -52,8 +52,6 @@ class ViewController: UIViewController {
         
         print(" ----------- now in received")
         
-        message = 4
-        
         // continue playing if the message has been received immediately before
         if (currentAudio == audioLib[message]) {
             return
