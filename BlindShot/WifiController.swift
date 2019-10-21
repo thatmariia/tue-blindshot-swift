1a3c6ea0935c84acc0363f697513412764461c44
Mariia Turchina <mariia.turchina@gmail.com>
2019-10-21 21:50:14 +0200

    added url fetch with random test

--- BlindShot/WifiController.swift
+++ BlindShot/WifiController.swift
@@ -11,13 +11,27 @@ import Foundation
 extension ViewController {
     
     func fetchAdvice () {
-        let urlString =  "https://myurl.com"
+        let urlString =  "http://192.168.1.211:8080/blindshot"
         
         if let url = URL(string: urlString) {
             do {
                 let content = try String(contentsOf: url)
                 print("**** content : ", content)
-                message = Int(content) ?? 7
+                
+                var contentFirst : Character = "7"
+                if (content != ""){
+                    contentFirst = strIndex(str: content, index: 0)
+                }
+
+                print("**** content first : ", contentFirst)
+                
+                message = Int(String(contentFirst)) ?? 7
+                if ((message < 0) || (message > 7)){
+                    message = 7
+                }
+                
+                print("**** message : ", message)
+        
                 received()
             } catch {
                 print("content could not be loaded")
@@ -29,18 +43,23 @@ extension ViewController {
     
     func setTimer () {
         timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(fire(timer:)), userInfo: nil, repeats: true)
-        secLeft = 600
+        //secLeft = 600
         print("TIMER SET")
     }
     
     @objc func fire(timer: Timer)
     {
-        secLeft -= 1
+        //secLeft -= 1
         fetchAdvice()
         
-        if (secLeft <= 0){
+        /*if (secLeft <= 0){
             timer.invalidate()
             print("TIMER INVALIDATED")
-        }
+        }*/
+    }
+    
+    func strIndex (str : String, index : Int) -> Character {
+        let i = str.index(str.startIndex, offsetBy: index)
+        return str[i]
     }
 }
